195c90625bb27ed91f1271f9dcc140143ac45b1e
Dan Laush <dan.laush@gmail.com>
2016-04-15 15:44:50 +1000

    JS lint fixes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,11 +1,17 @@
-$(document).ready(function() {
-	$('#modal').on('show.bs.modal', function (event) {
-	  var button = $(event.relatedTarget) // Button that triggered the modal
-	  var title = button.data('title') // Extract info from data-* attributes
-	  var text = button.data('caption')
-	  
-	  var modal = $(this)
-	  modal.find('#modalLabel').text(title)
-	  modal.find('#modalText').text(text)
-	})
-});
+/*global $ */
+
+(function() {
+	'use strict';
+
+	$(document).ready(function() {
+		$('#modal').on('show.bs.modal', function (event) {
+			var button = $(event.relatedTarget); // Button that triggered the modal
+			var title = button.data('title'); // Extract info from data-* attributes
+			var text = button.data('caption');
+
+			var modal = $(this);
+			modal.find('#modalLabel').text(title);
+			modal.find('#modalText').text(text);
+		});
+	});
+}());
